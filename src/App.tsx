6a423bc60ca00cbb343fcf1df41a51b9c6001fca
Andrei <aaroller669@gmail.com>
2023-02-06 21:24:27 +0900

    responsive version updated for coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
 import ComingSoon from "_scenes/comingsoon"
 
 function App() {
