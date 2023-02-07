85df2e38433a95f974e657d69760601482eee010
Andrei <aaroller669@gmail.com>
2023-02-07 20:21:59 +0900

    social links added

--- src/scenes/comingsoon/index.tsx
+++ src/scenes/comingsoon/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import "_styles/comingsoon.scss"
 import logo from '_images/logo-white.png'
 import {ReactComponent as Headphone} from '_images/headphone.svg';
+import SocialLinks from '@/componenets/atoms/sociallnk';
 
 export default function ComingSoon(){
 
@@ -17,9 +18,14 @@ export default function ComingSoon(){
             <div className = 'body'>
                 <h1>COMING SOON!</h1>
                 <h2>Online counselling at your convenience</h2>
+                <div className='email-subscribe'>
+                    <input type='text' placeholder='Enter Email Address'/>
+                    <button><span>Subscribe</span></button>
+                </div>
             </div>
 
             <div className = 'footer'>
+                <SocialLinks/>
                 <div className = 'copyright'>
                     <p>© 2023 LSSYA™ - The pioneer online psychotherapy platform in Ethiopia</p>
                 </div>
